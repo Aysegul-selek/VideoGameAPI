4508a0d902262fa9c4c3df1fbfcd2326c3b27129
Aysegul-selek <aysegul.selek03@gmail.com>
2025-01-20 20:13:31 +0300

    Add project files.

---
+++ Controllers/VideoGameController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VideoGameAPI.Controllers
+{
+    public class VideoGameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
