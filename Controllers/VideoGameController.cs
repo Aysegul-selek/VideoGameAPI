d90fa5361129f314ad4fff631a4c17622786480d
Aysegul-selek <aysegul.selek03@gmail.com>
2025-01-21 15:56:42 +0300

    Iplement Crud and Send Data with migration

--- Controllers/VideoGameController.cs
+++ Controllers/VideoGameController.cs
@@ -1,12 +1,64 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VideoGameAPI.Data;
+using VideoGameAPI.Entity;
 
 namespace VideoGameAPI.Controllers
 {
-    public class VideoGameController : Controller
+    [ApiController]
+    [Route("[controller]")]
+    public class VideoGameController(VideoGameDbContext context) : ControllerBase
     {
-        public IActionResult Index()
+        private readonly VideoGameDbContext _context = context;
+
+        [HttpGet]
+        public async Task<ActionResult<List<VideoGame>>> GetVideoGames()
         {
-            return View();
+            return Ok(await _context.VideoGames.ToListAsync());
         }
-    }
+
+        [HttpGet("{id}")]
+        public async  Task<ActionResult<VideoGame>>GetVideoGameById(int id)
+        {
+            var game =await _context.VideoGames.FindAsync(id);
+            if (game == null)
+                return NotFound();
+            return Ok(game);
+        }
+        [HttpPost]
+        public async Task<ActionResult<VideoGame>> AddVideoGame(VideoGame newGame)
+        {
+            if (newGame is null)
+                return BadRequest();;
+            _context.Add(newGame);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetVideoGameById), new { id = newGame.Id }, newGame);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateVideoGame(int id, VideoGame newGame)
+        {
+            var game = await _context.VideoGames.FindAsync(id);
+            if (game is null)
+                return BadRequest();
+
+            game.Title = newGame.Title;
+            game.Platform = newGame.Platform;
+            game.Developer = newGame.Developer;
+            game.Publisher = newGame.Publisher;
+            await  _context.SaveChangesAsync();
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task <IActionResult> DeleteVideoGame(int id)
+        {
+            var game = await _context.VideoGames.FindAsync( id);
+            if (game is null)
+                return BadRequest();
+
+           _context.VideoGames.Remove(game);
+            await _context.SaveChangesAsync();
+            return NoContent();
+
+            }
+        }
 }
---
+++ Data/VideoGameDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VideoGameAPI.Entity;
+
+namespace VideoGameAPI.Data
+{
+    public class VideoGameDbContext(DbContextOptions<VideoGameDbContext>options):DbContext(options)
+    {
+        public DbSet<VideoGame> VideoGames { get; set; }
+    }
+}
